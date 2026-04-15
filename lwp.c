741c3d0283e8ca283fb5cbf2a415a7b7b8c592f6
Ella Linden Yam <elyam@unix1.csc.calpoly.edu>
2026-04-14 23:40:02 -0700

    scheduler funcs

--- lwp.c
+++ lwp.c
@@ -1,4 +1,86 @@
+#include "lwp.h"
 
+/* SCHEDULER */
+/* sched_one is PREVIOUS
+   sched_two is NEXT */
+static thread head = NULL;
+
+void init() {
+    /* don't need? */
+}
+
+void shutdown() {
+    /* don't need? */
+}
+
+void admit(thread new) {
+    if(head == NULL) {
+        return NULL;
+    }
+
+    thread old_end = head->sched_one;
+    head->sched_one = new;
+    new->sched_one = old_end:
+    new->sched_two = head;
+    old_end->sched_two = new;
+}
+
+void remove(thread victim) {
+    thread current = head;
+
+    while(current != head) {
+        if(current == victim) {
+            left = victim->sched_one;
+            right = victim->sched_two;
+            left->sched_two = right;
+            right->sched_one = left;
+
+            victim->sched_one = NULL;
+            victim->sched_two = NULL;
+
+            break;
+        }
+        current = current->sched_two;
+    }
+
+    /* add error for if reaching end of list without finding */
+}
+
+thread next() {
+    if(head == NULL) {
+        return NULL;
+    }
+
+    head = head->sched_two;
+    return head->sched_one;   
+}
+
+int qlen() {
+    /* count var
+    count as traversing
+    return count*/
+
+    int count = 1;
+    thread current = head->sched_two;
+
+    while(current != head) {
+        count = count + 1;
+    }
+
+    return count;
+}
+
+typedef struct scheduler {
+    void init();
+    void shutdown();
+    void admit(thread new);
+    void remove(thread victim);
+    thread next();
+    int qlen();
+} Scheduler;
+
+
+/* LWP */
 tid_t lwp_create(lwpfun function, void *argument) {
     /* */
     return;
